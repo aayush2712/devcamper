88b199b0137b7704778c14be4f64a836339e170c
grimmie <aayush2712@gmail.com>
2022-09-02 01:50:51 +0530

    advancedResult

--- routes/courses.js
+++ routes/courses.js
@@ -8,8 +8,20 @@ const {
   deleteCourses,
 } = require('../controllers/courses');
 
+const Course = require('../models/Course');
+const advancedResults = require('../middleware/advancedResults');
+
 const router = express.Router({ mergeParams: true });
-router.route('/').get(getCourses).post(addCourses);
+router
+  .route('/')
+  .get(
+    advancedResults(Course, {
+      path: 'bootcamp',
+      select: 'name description',
+    }),
+    getCourses
+  )
+  .post(addCourses);
 router.route('/:id').get(getCourse).put(updateCourses).delete(deleteCourses);
 
 module.exports = router;
